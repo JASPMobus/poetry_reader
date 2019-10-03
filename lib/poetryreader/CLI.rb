29aa9550e69241d652efe82da7cdc27aa5c738ca
JASPMobus <49496054+JASPMobus@users.noreply.github.com>
2019-10-03 16:50:52 -0400

    adds reading poem functionality
    Additionally cleans up the CLI run loop handling (adding exit conditions, etc.) and updates version to v0.2.0

--- lib/poetryreader/CLI.rb
+++ lib/poetryreader/CLI.rb
@@ -5,28 +5,37 @@ class CLI
     puts "PoetryReader uses the Poetry Foundation website to share poems with you! All you need to know is the author's name and the title!\nWhen inputting the author's name, be sure to put a space between each part, even if it's between two initials!"
 
     ask = ""
+    ask_for_poet = true
 
-    while ask != "y" do
-      puts "What author would you like to read a poem from?"
+    while true do
+      if ask_for_poet
+        puts "What author would you like to read a poem from?"
+        ask = gets.chop
 
-      ask = gets
+        if ask.downcase == "list poets" || ask.downcase == "list"
+          Poet.all.each { |poet| puts poet.name }
+        elsif ask == "exit"
+          exit!
+        else
+          poet = Poet.find_or_create_new(ask)
 
-      poet = Poet.find_or_create_new(ask)
+          ask_for_poet = false
+        end
+      else
+        puts "Which of #{poet.name}'s poems would you like to read?"
+        ask = gets.chop
 
-      #lists the poet's poems
-      poet.poems.each do |poem|
-        puts poem.title
+        if ask.downcase == "list poems" || ask.downcase == "list"
+          poet.poems.each { |poem| puts poem.title }
+        elsif ask == "exit"
+          exit!
+        else
+          Poem.read(ask, poet)
+          puts ""
+
+          ask_for_poet = true
+        end
       end
-
-      puts "Which of their poems (above) would you like to read?"
-
-      ask = gets
-
-      Poem.read(ask, poet)
-
-      puts "Would you like to end? y/(n)"
-
-      ask = gets
     end
   end
 end
--- lib/poetryreader/poem.rb
+++ lib/poetryreader/poem.rb
@@ -25,13 +25,9 @@ class Poem
   end
 
   def self.read(title, author)
-    poem = Poem.all.find { |poem| poem.author == author }
+    poem = Poem.all.find { |poem|  poem.title.downcase == title.downcase }
 
-    if !poem
-      puts poem
-    else
-      poem.read
-    end
+    poem.read
   end
 
   def noko
@@ -39,6 +35,8 @@ class Poem
   end
 
   def read
-    self.noko.css("div")
+    self.noko.css("div[style='text-indent: -1em; padding-left: 1em;']").each do |line|
+      puts line.text
+    end
   end
 end
